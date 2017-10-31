b4d5f2d1235570ba74df27f0ac3da6789de6aa6e
Brian Menges <mengesb@gmail.com>
2017-10-30 17:23:32 -0700

    v1.0.0 Chef Compliance Cookbook * Update chef-ingredient dependency to `2.1.10` per #22 * Update `compliance_fqdn` to `fqdn` per #23 (https://docs.chef.io/config_rb_compliance.html) * Update to dokken images for testing * Adding top-level attribute for `topology` and logic in the template to accomidate

--- metadata.rb
+++ metadata.rb
@@ -4,9 +4,9 @@ maintainer_email 'help@sous-chefs.io'
 license 'Apache 2.0'
 description 'Installs/Configures chef-compliance'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version '0.1.6'
+version '1.0.0'
 
-depends 'chef-ingredient', '>= 0.18.4'
+depends 'chef-ingredient', '>= 2.1.10'
 
 source_url 'https://github.com/sous-chefs/chef-compliance' if respond_to?(:source_url)
 issues_url 'https://github.com/sous-chefs/chef-compliance/issues' if respond_to?(:issues_url)
