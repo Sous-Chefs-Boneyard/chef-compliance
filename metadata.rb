62c28eb1c8c3dd15f234e6359bd762d1a59a2996
Dan Webb <dan.webb@damacus.io>
2018-03-06 09:48:18 +0000

    Add dokken and delivery

--- metadata.rb
+++ metadata.rb
@@ -1,12 +1,12 @@
-name 'chef-compliance'
-maintainer 'Sous Chefs'
+name             'chef-compliance'
+maintainer       'Sous Chefs'
 maintainer_email 'help@sous-chefs.io'
-license 'Apache 2.0'
-description 'Installs/Configures chef-compliance'
+license          'Apache-2.0'
+description      'Installs/Configures chef-compliance'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+source_url       'https://github.com/sous-chefs/chef-compliance' if respond_to?(:source_url)
+issues_url       'https://github.com/sous-chefs/chef-compliance/issues' if respond_to?(:issues_url)
+
 version '0.1.6'
 
 depends 'chef-ingredient', '>= 0.18.4'
-
-source_url 'https://github.com/sous-chefs/chef-compliance' if respond_to?(:source_url)
-issues_url 'https://github.com/sous-chefs/chef-compliance/issues' if respond_to?(:issues_url)
