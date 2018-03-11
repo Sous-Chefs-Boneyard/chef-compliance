cc31acbda8516a2ed180c269f22a34f1456da9ff
Dan Webb <dan.webb@damacus.io>
2018-03-11 21:39:05 +0000

    Kitchen doesn't seem to be able to set the hostname right now so disabling the check

--- metadata.rb
+++ metadata.rb
@@ -6,7 +6,7 @@ description      'Installs/Configures chef-compliance'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 source_url       'https://github.com/sous-chefs/chef-compliance' if respond_to?(:source_url)
 issues_url       'https://github.com/sous-chefs/chef-compliance/issues' if respond_to?(:issues_url)
-chef_version     '12.5'
+chef_version     '>= 12.5'
 
 version '0.1.6'
 
